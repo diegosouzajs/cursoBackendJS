ac175774ec198a319944fe3dd5c6c3d893832c22
DIEGO DE SOUZA RODRIGUES <srdiegorodrigues@gmail.com>
2025-10-02 16:39:41 -0300

    Inclusão de observações sobre o conteúdo da aula

--- mod02LogDeProgramacao/introJavascript/aula01/tema01.js
+++ mod02LogDeProgramacao/introJavascript/aula01/tema01.js
@@ -1,11 +1,17 @@
-// Aula 01 - console.log e comentários
-// este é um comentário de uma linha
-/* Abertura
-Este é um comentário de bloco
-Tudo que estiver aqui será ignorado pela engine do javascript
-Não importa a quantida de linhas. Este e
+/*
+Aula 01 - console.log e comentários
 
-Fechamento*/
+console.log é uma função javascript que permite imprimir conteúdo no console.
+Sua chamada é da seguinte forma:
+    console.log(conteudo);
+
+- Onde "conteúdo" pode ser um valor literal bruto , variavel,
+- constante, objetos, etc. Ou seja, podemos imprimir qualquer coisa com o
+     console.log
+- Cada console.log insere, automaticamente, uma quebra de linha em seu final.
+*/
+
+// Formas de passar string literal para o console.log
 console.log('Olá, mundo!');
 console.log("Olá, mundo com aspas duplas!");
 console.log(`Olá, mundo com aspas duplas!`);
@@ -15,13 +21,32 @@ console.log(`Olá, mundo com aspas duplas!`);
 console.log('Olá, "mundo"'); //aspas duplas
 console.log("Olá, 'mundo'"); //aspas duplas
 console.log(`"Olá", 'mundo'`); //aspas duplas e aspas simples
+console.log("Olá, \"mundo\""); //aspas duplas e aspas simples
+console.log('Olá, \'mundo\''); //aspas duplas e aspas simples
 
+
+/* 
+Comentários
+
+Comentários são trechos do código que são ignorados pelos motores do javascript
+Uma linha comentada não é utilizada na execução do algoritimo
+Um comentário nunca é exibido para o usuário
+Comentários são uteis para explicar para outros desenvolvedores 
+e até para nós mesmos, sobre o que o código específico faz
+/*
+
+// este é um comentário de uma linha
+
+/*
+Este é um comentário de bloco
+Tudo que estiver aqui será ignorado pela engine(motor) do javascript.
+Não importa a quantida de linhas. 
+*/
 
 /**
  * Este tipo de comentário em bloco é comumente usado na linguagem java.
  * É adotado quando você deseja documentar formalmente o código
  * para que outras pessoas (ou ferramentas) entendam sua finalidade.
  * Ele não é apenas um comentário para programadores, mas um meio de gerar 
- * documentação oficial automaticamente.
- * 
+ * documentação oficial automaticamente (para a linguagem java). 
  */
--- mod02LogDeProgramacao/introJavascript/aula01/tema02.js
+++ mod02LogDeProgramacao/introJavascript/aula01/tema02.js
@@ -4,16 +4,55 @@ Variaveis são nomes atribuidos a endereços de memória que podem
 passar por uma reatribuição durante a execução de um código. É possível criar
 uma variavel sem atribuir um valor a ela
     let nome;
+    
+Abaixo são listados alguns pontos importantes sobre variáveis
+     * não podemos declarar a mesma variavel mais de uma vez
+     * não podemos criar variaveis com palavras reservadas, como let, number, 
+     * var, console, const, func, if, else, entre várias outras opções
+     * variaveis precisam ter nomes significativos
+     * variaveis não podem iniciar seu nome com número
+     * recomenda-se que variaveis iniciem com letra minuscula
+     * nomes delas não podem conter espaços ou traços 
+     * let nome cliente ou let nome-cliente
+     * são case-sensitive (como tudo no javascript)
+     * variaveis com nomes compostos é bom utilizar a notação camelCase
+     * cada palavra posterior a primeira, inicia com a letra maiuscula
+     * as variaveis são case-sensitive (letra maiuscula e letra minuscula
+     * fazem diferença)
+     * não podemos redeclarar variaveis com let ou var
+     * NÃO UTILIZE VAR, UTILIZE LET
+
 
 Constantes, diferentemente das variaveis, não permite a reatribuição de valores.
 Ao criar uma constante, sempre devemos atribuir um valor a ela.
     const nome = "Diego"; -> certo
     const sobrenome; -> errado
 
+    Abaixo são listados alguns pontos importantes sobre constantes
+    * Constantes são variaveis que não mudam o valor na execução do código
+    * Não podemos criar constantes com palavras reservadas
+    * não podemos começar constantes com um número
+    * Constantes devem ter nomes significativos
+    * não podem conter espaços ou traços
+    * utilizamos camelCase como padrão de escrita
+    * são case-sensitive (como tudo no javascript)
+    * não podemos modificar o valor de uma constante
+    * a constante precisa ser criada e inicializada ao mesmo tempo
+    * const nome; //errado
+    * const nome = 'diego' // certo
+    * const assegura que o valor da variavel não seja modificado no decorrer do
+    * código. Quando for necessário modificar o valor de uma variavel, utilize 
+    * sempre o let
+    * NÃO UTILIZE VAR, UTILIZE CONST 
+
 Quando usar?
     Use const para tudo que puder
     Use let quando precisar
     Evite usar o var pois trata-se de um código legado
+
+Ao criar uma variavel ou constante não é necessário definir o tipo dela.
+Por ser uma linguagem de tipagem dinâmica somente a inclusão do valor fará
+com que o javascript já defina o seu tipo automaticamente.
 */
 // declaração de variável
 var carro = "Fuscão preto"; // legado -> importante evitar o uso
