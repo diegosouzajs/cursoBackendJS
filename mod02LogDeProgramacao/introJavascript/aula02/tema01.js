5a923184c004d5c7824c1b46394f551dec86a151
DIEGO DE SOUZA RODRIGUES <srdiegorodrigues@gmail.com>
2025-10-06 13:11:27 -0300

    Descrição do typeof

--- mod02LogDeProgramacao/introJavascript/aula02/tema01.js
+++ mod02LogDeProgramacao/introJavascript/aula02/tema01.js
@@ -17,7 +17,10 @@ uma vez criados, não podem ser alterados — só substituídos.
     let big = 123456789012345678901234567890n;
 
 Para verificar o tipo de um dado, usamos o método typeof
-    console.log(typeof nome, nome)
+    typeof é um operador que retorna uma string indicando o tipo de dado de uma
+    variável ou expressão. Ele é muito útil para verificar o tipo de uma 
+    variável antes de fazer operações com ela, ajudando
+        console.log(typeof nome, nome)
 */
 
 let nome = "Diego";
