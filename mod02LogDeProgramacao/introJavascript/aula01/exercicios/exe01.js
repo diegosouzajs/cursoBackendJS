3aae6cbd811a58b75759b2e41cc595a06422ae56
DIEGO DE SOUZA RODRIGUES <srdiegorodrigues@gmail.com>
2025-10-09 10:07:27 -0300

    Inclusão de observações no arquivo exe01.js da aula 01

--- mod02LogDeProgramacao/introJavascript/aula01/exercicios/exe01.js
+++ mod02LogDeProgramacao/introJavascript/aula01/exercicios/exe01.js
@@ -6,9 +6,22 @@ mas utilizando os dados de suas variáveis:
 
 Diego Rodrigues tem 39 anos, pesa 80 kg. Tem 1.72 de altura e seu imc é de 
 27,041644
- 
-*/
 
+*/
+/**
+ * IMPORTANTE
+ * Para importar um arquivo javascript para dentro de um html
+ * utilizamos a tag script do html
+ * 
+ * Entre as tags body do html, devemos inserir, o mais próximo possível da última
+ * o seguinte:
+ * <script src='caminhoDoArquivo/nomeDoArquivo.js"></script>
+ * Onde:
+ *      - caminhoDoArquivo representa o diretório onde seu script está salvo
+ *        caso esteja dentro da mesma pasta que o arquivo html, não é necessário informa-lo
+ *      - nomeDoArquivo.js representa a nomeação do arquivo em que salvou o código javascript 
+ *        a ser importado.
+ */
 
 
 const nome = "Diego";
