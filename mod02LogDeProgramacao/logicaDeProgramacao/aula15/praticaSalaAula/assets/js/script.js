4913c25fc67cf3b26cca55f783dc5780e0e1b68e
srdiegorodrigues <srdiegorodrigues@gmail.com>
2025-11-05 21:46:40 -0300

    Inclusão do arquivo script.js na pasta js da aula prática - 05/11/2025

---
+++ mod02LogDeProgramacao/logicaDeProgramacao/aula15/praticaSalaAula/assets/js/script.js
@@ -0,0 +1,25 @@
+/* Função para verificar a estação do ano 
+Elabore uma função que verifica a estação do ano conforme o mês
+O usuário deverá inserir um mês em um campo no html do tipo input
+(não usar o prompt). Com o valor inserido você deverá verificar 
+qual é a estação do ano do referido mês. Caso o valor informado não 
+corresponda a um mês válido, deve retornar "Valor inválido".
+Abaixo são listadas as estações e os seus respectivos meses
+
+As estações do ano no Brasil (hemisfério sul) são: 
+outono (março a junho), inverno (junho a setembro), 
+primavera (setembro a dezembro) e verão (dezembro a março). 
+Para fins meteorológicos, a divisão é mais simplificada: 
+o verão são os meses de dezembro, janeiro e fevereiro; 
+o outono, março, abril e maio; o inverno, junho, julho e agosto; 
+e a primavera, setembro, outubro e novembro. 
+
+const inverno = ['junho', 'julho', 'agosto'];
+const primavera = ['setembro', 'outubro', 'novembro'];
+const verao = ['dezembro', 'fevereiro', 'janeiro'];
+const outono = ['março', 'abril', 'maio'];
+
+*/
+
+
+
