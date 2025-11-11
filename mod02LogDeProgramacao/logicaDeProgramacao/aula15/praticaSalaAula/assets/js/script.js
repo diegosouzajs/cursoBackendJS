e8c7a9267ba884e6ebe527bdf8bdf2e5bf72dc69
DIEGO DE SOUZA RODRIGUES <srdiegorodrigues@gmail.com>
2025-11-11 08:18:27 -0300

    Atualização dos conteúdos da prática em sala de aula

--- mod02LogDeProgramacao/logicaDeProgramacao/aula15/praticaSalaAula/assets/js/script.js
+++ mod02LogDeProgramacao/logicaDeProgramacao/aula15/praticaSalaAula/assets/js/script.js
@@ -6,14 +6,6 @@ qual é a estação do ano do referido mês. Caso o valor informado não
 corresponda a um mês válido, deve retornar "Valor inválido".
 Abaixo são listadas as estações e os seus respectivos meses
 
-As estações do ano no Brasil (hemisfério sul) são: 
-outono (março a junho), inverno (junho a setembro), 
-primavera (setembro a dezembro) e verão (dezembro a março). 
-Para fins meteorológicos, a divisão é mais simplificada: 
-o verão são os meses de dezembro, janeiro e fevereiro; 
-o outono, março, abril e maio; o inverno, junho, julho e agosto; 
-e a primavera, setembro, outubro e novembro. 
-
 const inverno = ['junho', 'julho', 'agosto'];
 const primavera = ['setembro', 'outubro', 'novembro'];
 const verao = ['dezembro', 'fevereiro', 'janeiro'];
@@ -21,5 +13,91 @@ const outono = ['março', 'abril', 'maio'];
 
 */
 
+// if(mes === 'março' || mes === 'abril' || mes == 'maio'){
+//  console.log('Outono');
+//}
 
+// const mes = 'março'
+
+// switch (mes){
+//     case 'março':
+//     case 'abril':
+//     case 'maio':
+//         console.log('Outono');
+//         break;
+//     default:
+//         console.log('Mês inválido!');
+// }
+
+function verEstacao(){
+    const inverno = ['junho', 'julho', 'agosto'];
+    const primavera = ['setembro', 'outubro', 'novembro'];
+    const verao = ['dezembro', 'fevereiro', 'janeiro'];
+    const outono = ['março', 'abril', 'maio'];
+    const form = document.querySelector('form');
+    
+    console.log(mes);
+    const resposta = document.querySelector('#resposta');
+    
+    function imprimirEstacao(evento){
+        evento.preventDefault();
+        const mes = document.querySelector('#mes').value.toLowerCase();
+        if(inverno.includes(mes)) {
+            resposta.innerHTML = `<p class="success">A estação do mês ${mes} é INVERNO</p>`;
+        }else if (primavera.includes(mes)){
+            resposta.innerHTML = `<p class="success">A estação do mês ${mes} é PRIMAVERA</p>`;
+        }else if (verao.includes(mes)){
+            resposta.innerHTML = `<p class="success">A estação do mês ${mes} é VERÃO</p>`;
+        }else if (outono.includes(mes)){
+            resposta.innerHTML = `<p class="success">A estação do mês ${mes} é OUTONO</p>`;
+        }else{
+            resposta.innerHTML = `<p class="error">Valor inválido! ${mes}</p> `;
+        }
+    }
+    form.addEventListener('submit', imprimirEstacao);
+}
+
+verEstacao();
+
+
+
+
+// function verEstacaoMes(){
+//     const inverno = ['junho', 'julho', 'agosto'];
+//     const primavera = ['setembro', 'outubro', 'novembro'];
+//     const verao = ['dezembro', 'fevereiro', 'janeiro'];
+//     const outono = ['março', 'abril', 'maio'];
+    
+//     //capturar os valores do meu formulário e o campo que receberá as respostas
+//     const resposta = document.querySelector('#resposta')
+//     const form = document.querySelector('form');
+
+//     // função para imprimir na tela
+//     function imprimirValor(evento){
+//         // recebe o evento e evita o seu comportamento padrão
+//         evento.preventDefault();
+//         const mes = form.querySelector('#mes').value;
+//         console.log(mes);
+//         if (primavera.includes(mes.toLowerCase())){
+//             resposta.innerHTML = `<p class="success">A estação no mês de ${mes} é PRIMAVERA</p>`;
+//         } 
+//         else if (verao.includes(mes.toLowerCase())){
+//             resposta.innerHTML = `<p class="success">A estação no mês de ${mes} é VERÃO</p>`;
+//         } 
+//         else if (outono.includes(mes.toLowerCase())){
+//             resposta.innerHTML = `<p class="success">A estação no mês de ${mes} é OUTONO</p>`;
+//         } 
+//         else if (inverno.includes(mes.toLowerCase())){
+//             resposta.innerHTML = `<p class="success">A estação no mês de ${mes} é INVERNO</p>`;
+//         } else {
+//             resposta.innerHTML = `<p class="success">O valor ${mes} não é um mês válido</p>`;    
+//         }        
+//     }
+    
+//     // adiciono uma espécie de escuta ao meu formulário que ao ser submetido, não
+//     // recarregará a página, mas sim envia para a função imprimirValor
+//     form.addEventListener('submit', imprimirValor);
+// }
+
+// verEstacaoMes();
 
