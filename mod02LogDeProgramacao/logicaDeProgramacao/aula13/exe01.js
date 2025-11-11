0672299c158cbbff0c6f3c94fd3663e3295db0f4
DIEGO DE SOUZA RODRIGUES <srdiegorodrigues@gmail.com>
2025-11-11 08:16:36 -0300

    Atualização do exercício do conteúdo

--- mod02LogDeProgramacao/logicaDeProgramacao/aula13/exe01.js
+++ mod02LogDeProgramacao/logicaDeProgramacao/aula13/exe01.js
@@ -1,42 +1,72 @@
 /* 
-Cenário: análise de notas de estudantes
+Você recebeu uma lista de objetos representando alunos e suas respectivas notas 
+em uma disciplina.
+Seu objetivo é analisar os resultados e obter informações estatísticas 
+utilizando métodos de arrays (filter, map, reduce).
 
-Suponha que temos uma lista de alunos com suas respectivas notas em uma
-disciplina.
-Queremos calcular a média das notas dos alunos aprovados, considerando apenas 
-aqueles com nota ≥ 7.
+const alunos = [
+    { nome: 'Diego', nota: 9.0 },
+    { nome: 'Ana', nota: 6.5 },
+    { nome: 'Lucas', nota: 7.2 },
+    { nome: 'Mariana', nota: 8.3 },
+    { nome: 'João', nota: 5.9 },
+];
+
+Filtrar apenas os alunos aprovados, considerando nota maior ou igual a 7.
+Gerar um novo array contendo apenas as notas dos alunos aprovados.
+Calcular a média das notas dos aprovados.
+Exibir no console:
+A lista dos aprovados;
+As notas dos aprovados;
+A média final com duas casas decimais.
 */
 // cada objeto tem duas propriedades
 // nome e nota
 
 const alunos = [
-  { nome: "Ana", nota: 9 },
-  { nome: "Bruno", nota: 5 },
-  { nome: "Carla", nota: 8 },
-  { nome: "Diego", nota: 7.5 },
-  { nome: "Fernanda", nota: 10 }
+    { nome: 'Diego', nota: 9.0 },
+    { nome: 'Ana', nota: 7.5 },
+    { nome: 'Lucas', nota: 7.2 },
+    { nome: 'Mariana', nota: 8.3 },
+    { nome: 'João', nota: 5.9 },
 ];
 
-// Filtrar apenas os aprovados (nota >= 7)
-
+// Filtrar apenas os aprovados (nota >= 7) usando filter
 const aprovados = alunos.filter(aluno => aluno.nota >= 7);
-console.log('Lista de aprovado:', aprovados);
+console.log('Lista de aprovados: ',aprovados);
 
-// Extrair apenas as notas dos aprovados
-const notas = aprovados.map(aprovado => aprovado.nota );
-console.log('Notas dos aprovados: ', notas)
+// Extrair apenas as notas dos aprovados usando map
+
+const notasAprovados = aprovados.map(aprovado => aprovado.nota);
+console.log('Notas dos aprovados: ',notasAprovados);
 
 // Calcular a média das notas usando reduce
 
-const media = notas.reduce((acum, nota) => (acum += nota), 0) /notas.length;
+const media = notasAprovados.reduce((acc, nota) => acc+nota,0) / notasAprovados.length;
+console.log('Nota média dos aprovados:',media.toFixed(2));
+console.log('------------------------');
 
-console.log('Média de notas dos aprovados', media.toFixed(2));
-const nome = 'Diego'
-console.log(nome.indexOf('e'));
 
-console.log('Aprovados',aprovados);
-console.log('Notas dos aprovados',notasAprovados);
-console.log(`Média dos aprovados: ${media.toFixed(2)}`);
+// // Filtrar apenas os aprovados (nota >= 7)
+
+// const aprovados = alunos.filter(aluno => aluno.nota >= 7);
+// console.log('Lista de aprovado:', aprovados);
+
+// // Extrair apenas as notas dos aprovados
+// const notas = aprovados.map(aprovado => aprovado.nota );
+// console.log('Notas dos aprovados: ', notas)
+
+// // Calcular a média das notas usando reduce
+
+// const media = notas.reduce((acum, nota) => (acum += nota), 0) /notas.length;
+
+// console.log('Média de notas dos aprovados', media.toFixed(2));
+// const nome = 'Diego'
+// console.log(nome.indexOf('e'));
+
+// console.log('Aprovados',aprovados);
+// console.log('Notas dos aprovados',notasAprovados);
+// console.log(`Média dos aprovados: ${media.toFixed(2)}`);
 
 // método avançado para calcular média
 console.log('---------------------------------')
